01cb9baf068c915031827e1ebc049480fcc952f6
Ricardo Marques <contato.ricardotr@gmail.com>
2025-04-21 12:35:11 -0300

    learn (ap05-react): encerra a secao 2.5

---
+++ estacao-climatica-classes/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state ={
+      latitude: null,
+      longitute: null,
+      estacao: null,
+      data: null,
+      icona: null
+    }
+  }
+
+  render(){
+
+    return (
+      <div>Meu App</div>
+
+
+    )
+
+  }
+  
+}
+
+export default App
