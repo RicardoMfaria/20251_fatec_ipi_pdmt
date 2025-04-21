20d5583c30cf3a32b534a8244722c273f3d577e8
Ricardo Marques <contato.ricardotr@gmail.com>
2025-04-21 18:00:32 -0300

    learn(ap06-react): encerra a apostila

--- estacao-climatica-classes/src/App.jsx
+++ estacao-climatica-classes/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { EstacaoClimatica } from './EstacaoClimatica'
+import Loading from "./Loading"
 
 
 class App extends React.Component{
@@ -28,6 +29,7 @@ class App extends React.Component{
 
   componentDidMount(){
     console.log('componenteDidMount')
+    this.obterLocalizacao()
   }
 
   componentDidUpdate(){
@@ -39,9 +41,7 @@ class App extends React.Component{
   }
 
   obterEstacao = (data, latitude) => {
-
     const anoAtual = data.getFullYear()
-
     //21/06
     const d1 = new Date(anoAtual, 5, 21)
     //24/09
@@ -51,9 +51,9 @@ class App extends React.Component{
     //21/03
     const d4 = new Date(anoAtual, 2, 21)
     const estaNoSul = latitude < 0
-    if (data >= d1 && data < d2)
+    if(data >= d1 && data < d2)
       return estaNoSul ? 'Inverno' : 'Verão'
-    if (data >= d2 && data < d3)
+    if(data >= d2 && data < d3)
       return estaNoSul ? 'Primavera' : 'Outono'
     if (data >= d3 || data < d4)
       return estaNoSul ? 'Verão' : 'Inverno'
@@ -61,10 +61,10 @@ class App extends React.Component{
   }
 
   icones = {
-    'Primavera' : 'seedling',
-    'Verão' : 'sun',
-    'Outono' : 'tree',
-    'Inverno' : 'snowflake'
+    'Primavera': 'seedling',
+    'Verão': 'sun',
+    'Outono': 'tree',
+    'Inverno': 'snowflake'
   }
 
   obterLocalizacao = () => {
@@ -73,10 +73,10 @@ class App extends React.Component{
         const dataAtual = new Date()
         const estacao = this.obterEstacao(dataAtual, posicao.coords.latitude)
         const icone = this.icones[estacao]
-
+    
         this.setState({
           latitude: posicao.coords.latitude,
-          longitute: posicao.coords.longitude,
+          longitude: posicao.coords.longitude,
           estacao: estacao,
           data: dataAtual.toLocaleTimeString(),
           icone: icone
@@ -98,14 +98,22 @@ class App extends React.Component{
       <div className='container mt-2'>
         <div className="row justify-content-center">
           <div className="col-md-8">
-            <EstacaoClimatica 
+            {
+              !this.state.latitude && !this.state.mensagemDeErro ?
+                <Loading mensagem='Informe se permite acesso à localização...'/>
+              :
+                this.state.mensagemDeErro ?
+                <p className="border rounded p-2 fs-1 text-center">
+                  É preciso dar permissão de acesso à localização, caso contrário não será possível exibir a sua estação climática
+                </p>
+              :
+              <EstacaoClimatica 
               icone={this.state.icone}
               estacao={this.state.estacao}
               latitude={this.state.latitude}
-              longitude={this.state.longitute}
-              data={this.state.data}
-              mensagemDeErro={this.state.mensagemDeErro}
+              longitude={this.state.longitude}
               obterLocalizacao={this.obterLocalizacao}/>
+            }
           </div>
         </div>
       </div>
--- estacao-climatica-classes/src/EstacaoClimatica.jsx
+++ estacao-climatica-classes/src/EstacaoClimatica.jsx
@@ -1,37 +1,56 @@
 import React, { Component } from 'react'
 
 export class EstacaoClimatica extends Component {
+
+    timer = null
+
+    state = {
+        data: null
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(() => {
+            this.setState({
+                data : new Date().toLocaleTimeString()
+            })
+        }, 1000)
+        console.log(this.timer)
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timer)
+    }
+    
     render() {
         return (
             <div className="card">
                 <div className="card-body">
 
-                    <div
+                <div
                         className="d-flex align-items-center border rounded mb-2"
                         style={{ height: '6rem' }}>
                         <i className={`fas fa-5x fa-${this.props.icone}`}></i>
-                        <p className="w-75 ms-3 text-center fs-1">{this.props.estacao}
-                        </p>
+                        {/* p.w-75.ms-3.text-center.fs-1 */}
+                        <p className="w-75 ms-3 text-center fs-1">{this.props.estacao}</p>
+
                     </div>
 
                     <div>
-                        <p className="text">
+                        <p className='text-center'>
+                            {/* renderização condicional */}
                             {
                                 this.props.latitude ?
-                                    `Coordenadas: ${this.props.latitude},${this.props.longitute}. Data: ${this.props.data}.`
+                                    `Coordenadas: ${this.props.latitude},${this.props.longitude}. Data: ${this.state.data}.`
                                     :
-                                    this.props.mensagemDeErro ?
-                                        `${this.props.mensagemDeErro}`
-                                        :
-                                        `Clique no botão para saber a sua estação climática`
+                                    `Clique no botão para saber a sua estação climática`
                             }
                         </p>
                     </div>
 
                     <button
-                        onClick={this.obterLocalizacao}
+                        onClick={this.props.obterLocalizacao}
                         className='btn btn-outline-primary w-100 mt-2'>
-                        Qual a minha estação ?
+                        Qual a minha estação?
                     </button>
 
                 </div>
