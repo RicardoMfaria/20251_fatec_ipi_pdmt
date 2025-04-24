c8744670ee80375923077c671654be65b68fa9c4
Ricardo Marques <contato.ricardotr@gmail.com>
2025-04-23 21:43:59 -0300

    feat(p1): exibe lista ficticia

--- cep-finder/src/components/App.jsx
+++ cep-finder/src/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Busca from './Busca';
+import LocalidadeLista from './LocalidadeLista';
 
 function App() {
   return (
     <div>
       <h1>Hello, P1</h1>
+      <Busca />
+      <LocalidadeLista />
     </div>
   );
 }
